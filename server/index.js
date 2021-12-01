0814741277d4e1c6bca7a95499a2e54a637bb11d
quynguyen321 <quy.ybu@gmail.com>
2021-12-01 11:37:16 -0600

    demos

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ const app = express()
 app.use(express.static('client'))
 
 app.get('/', () =>{
-    resizeBy.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 const port = process.env.PORT || 4040
 
-app.listen(4040, ()=> console.log(`server running on port ${port}`))
+app.listen(port, ()=> console.log(`server running on port ${port}`))
